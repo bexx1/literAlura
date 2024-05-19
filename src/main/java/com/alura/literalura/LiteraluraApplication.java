0efb10d21ca284216ca8ee28cbf4053fae19b5d6
bexx1 <rebecasfigueiro@gmail.com>
2024-05-19 11:55:52 -0300

    Creating a http request to call the api

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,4 +1,4 @@
-package com.example.literalura;
+package com.alura.literalura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/alura/literalura/LiteraluraApplicationTests.java
+++ src/test/java/com/alura/literalura/LiteraluraApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.literalura;
+package com.alura.literalura;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
